0f8a31056aa4ee0f171e5f55623c47795ecb51a9
Adam Page <adam.page@samtec.com>
2019-11-04 11:54:35 -0500

    Initial untested MDIO over 3-wire SPI.

--- pyrpio/mdio/__init__.py
+++ pyrpio/mdio/__init__.py
@@ -1,4 +1,5 @@
 from typing import List
+import spidev
 from pyrpio import rpiolib
 
 class MDIO:
@@ -53,3 +54,76 @@ class MDIO:
     def write_c45_registers(self, pad: int, dad: int, regs: List[int], vals: List[int]):
         ''' Write multiple registers in CLAUSE45. '''
         return [self.write_c45_register(pad, dad, reg, val) for reg, val in zip(regs, vals)]
+
+
+class MDIOSPI:
+    """Peform MDIO over SPI interface.
+    Requires MOSI and MISO to be tied together with external pull-up
+    Chip select is not used either since MDIO packet contains phy_addr
+    """
+    C22_FRAME = 0x01
+    C45_FRAME = 0x00
+    OP_C22_WR = 0x01
+    OP_C22_RD = 0x02
+    OP_C45_AD = 0x00
+    OP_C45_WR = 0x01
+    OP_C45_RD_INC = 0x02
+    OP_C45_RD = 0x03
+
+    def __init__(self, path: str='/dev/spi-1'):
+        self.path: str = path
+        self._bus: spidev.SpiDev()
+
+    def open(self, speed_hz: int=5000):
+        self._bus.open()
+        self._bus.max_speed_hz = speed_hz
+        self._bus.no_cs = True # Dont need chip select
+        self._bus.mode = 0 # Mode 0: Clock pulse on 1, posedge data valid
+        self._bus.threewire = True
+        self._bus.bits_per_word = 32
+
+    def close(self):
+        self._bus.close()
+
+    def mdio_flush(self):
+        # Send 32-bit 1s to flush
+        rst = self._bus.xfer((0xFFFFFFFF).to_bytes(4, byteorder='big'))
+
+    def mdio_xfer(self, st: int, op: int, pad: int, dad: int, tat: int=0x3, val: int = 0xFFFF):
+        # Construct 32-bit packet (16-bit header and 16-bit data)
+        hdr = ( st & 0x03) << 14 |\
+              ( op & 0x03) << 12 |\
+              (pad & 0x1F) <<  7 |\
+              (dad & 0x1F) <<  2 |\
+              (tat & 0x03) <<  0
+        packet = (hdr << 16) & (val & 0xFFFF)
+        # Flush (ignore input)
+        self.mdio_flush()
+        # Transfer packet (on reads we write fake 1's to get data back)
+        rst = self._bus.xfer(packet.to_bytes(4, byteorder='big'))
+        # Flush (ignore input)
+        self.mdio_flush()
+        # Extract 16-bit data regardless of operation
+        return int.from_bytes(rst, byteorder='big') & 0xFFFF
+
+    def read_c22_register(self, pad: int, reg: int):
+        # Read register in one transaction
+        val = self.mdio_xfer(MDIOSPI.C22_FRAME, MDIOSPI.OP_C22_RD, pad=pad, dad=reg)
+        return val
+
+    def read_c45_register(self, pad: int, dad: int, reg: int):
+        # Write register address
+        self.mdio_xfer(MDIOSPI.C45_FRAME, MDIOSPI.OP_C45_AD, pad=pad, dad=dad, val=reg)
+        # Read register data
+        val = self.mdio_xfer(MDIOSPI.C45_FRAME, MDIOSPI.OP_C45_RD, pad=pad, dad=dad)
+        return val
+
+    def write_c22_register(self, pad: int, reg: int, val: int):
+        # Read register in one transaction
+        self.mdio_xfer(MDIOSPI.C22_FRAME, MDIOSPI.OP_C22_WR, pad=pad, dad=reg, val=val)
+
+    def write_c45_register(self, pad: int, dad: int, reg: int, val: int):
+        # Write register address
+        self.mdio_xfer(MDIOSPI.C45_FRAME, MDIOSPI.OP_C45_AD, pad=pad, dad=dad, val=reg)
+        # Write register data
+        self.mdio_xfer(MDIOSPI.C45_FRAME, MDIOSPI.OP_C45_RD, pad=pad, dad=dad, val=val)
