cffbe69c6517245c19d8a4a899152bbe1b5e9c69
Adam Page <adam.page@samtec.com>
2021-02-19 17:18:56 -0500

    fix: Fallback to static version if importlib.metadata not available. fix: Update readme examples.

--- pyrpio/__init__.py
+++ pyrpio/__init__.py
@@ -1,8 +1,8 @@
 ''' PyRPIO module '''
-from importlib.metadata import version
 from pyrpio.types import RPIOConfigs
 
 try:
+    from importlib.metadata import version
     __version__ = version(__name__)
 except Exception:
-    __version__ = "0.0.0"
+    __version__ = "0.1.1"
